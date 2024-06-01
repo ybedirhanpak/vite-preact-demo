8adfdb71082038b0f313b23b46823ea35a8ab6b7
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2024-06-01 15:28:11 +0300

    Use signals

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
-import { useState } from "preact/hooks";
+import { computed, useSignal } from '@preact/signals';
+
 import preactLogo from "./assets/preact.svg";
 import viteLogo from "/vite.svg";
+
 import "./app.css";
 
 export function App() {
-    const [count, setCount] = useState(0);
+    const count = useSignal(0); // Create a signal with an initial value of 0
+    const doubleCount = computed(() => count.value * 2); // Create a computed signal
+
+    console.log("Render App component");
 
     return (
         <>
@@ -22,9 +27,10 @@ export function App() {
             </div>
             <h1>Vite + Preact</h1>
             <div class="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is a {count}
+                <button onClick={() => count.value++}>
+                    count is {count.value}
                 </button>
+                <p>Double Count: {doubleCount.value}</p>
                 <p>
                     Edit <code>src/app.tsx</code> and save to test HMR
                 </p>
